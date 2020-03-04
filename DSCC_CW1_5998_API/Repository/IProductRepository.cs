907586c0e2fb9afd4dce694ec223723e2a2d7b1d
JavokhirAbduganiev <jabduganiev3@students.wiut.uz>
2020-03-04 23:20:55 +0500

    commit

---
+++ DSCC_CW1_5998_API/Repository/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using DSCC_CW1_5998_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC_CW1_5998_API.Repository
+{
+    public interface IProductRepository
+    {
+        void InsertProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        void DeleteProduct(int productID);
+
+        Product GetProductByID(int id);
+
+        IEnumerable<Product> GetProducts();
+    }
+}
---
+++ DSCC_CW1_5998_API/Repository/ProductRepository.cs
@@ -0,0 +1,50 @@
+﻿using DSCC_CW1_5998_API.DbContexts;
+using DSCC_CW1_5998_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSCC_CW1_5998_API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int productID)
+        {
+            var product = _dbContext.Products.Find(productID);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int productID)
+        {
+            var prod = _dbContext.Products.Find(productID);
+            return prod;
+        }
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products;
+        }
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Save();
+        }
+    }
+}
